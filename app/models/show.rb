139ff684eb7334f1e6e8bcaff73294439cb7d6b7
Candice Parker <lexus2424@gmail.com>
2019-08-06 15:55:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,4 +16,14 @@ class Show < ActiveRecord::Base
     self.find_by(rating: lowest_rating)
   end
   
+  def self.popular_shows
+    self.where("rating > 5")
+end
+
+def self.shows_by_alphabetical_order
+    self.order(:name)
+end
+
+
+  
 end
